448b8b816baf1e5435135904ff9283f2358add91
Austin Shafer <ashafer@badland.io>
2020-04-28 21:44:11 -0400

    Remove stale linuxkpi and clean depend files

--- src/common/linuxkpi/gplv2/include/asm/set_memory.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ASM_SET_MEMORY_H
-#define _ASM_SET_MEMORY_H
-
-int set_memory_wb(unsigned long addr, int numpages);
-int set_memory_uc(unsigned long addr, int numpages);
-int set_memory_wc(unsigned long addr, int numpages);
-
-#endif
