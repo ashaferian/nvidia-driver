95c687492a34952c9962dbd4d571728602096be6
Austin Shafer <ashafer@badland.io>
2020-04-29 22:10:56 -0400

    Remove everything to update to 440.82

--- src/nvidia-drm/machine/bus.h
+++
@@ -1,6 +0,0 @@
-/*-
- * This file is in the public domain.
- */
-/* $FreeBSD: release/12.0.0/sys/amd64/include/bus.h 244191 2012-12-13 21:27:20Z jimharris $ */
-
-#include <x86/bus.h>
--- src/nvidia-drm/machine/sysarch.h
+++
@@ -1,6 +0,0 @@
-/*-
- * This file is in the public domain.
- */
-/* $FreeBSD: release/12.0.0/sys/amd64/include/sysarch.h 233209 2012-03-19 21:57:31Z tijl $ */
-
-#include <x86/sysarch.h>
